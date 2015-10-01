31c7903787aa974334b4eaabb70e30b3d4d8644e
yayshine <ys2657@columbia.edu>
2015-10-01 16:38:22 +0900

    버튼을 그리드 레이아웃에 넣어서 계산 기능을 하는 계산기를 만듬

--- app/src/main/java/com/yayshine/calculator/MainActivity.java
+++ app/src/main/java/com/yayshine/calculator/MainActivity.java
@@ -6,15 +6,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    protected int value = 0;
+    protected int first = 0;
+    protected int second = 0;
+    protected boolean isNewInput = true;
+    protected int operator = -1;
+    protected TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initClickListeners();
     }
 
     @Override
@@ -22,6 +28,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void initClickListeners() {
+        // set Click Listeners for buttons
+        findViewById(R.id.btn_0).setOnClickListener(this);
+        findViewById(R.id.btn_1).setOnClickListener(this);
+        findViewById(R.id.btn_2).setOnClickListener(this);
+        findViewById(R.id.btn_3).setOnClickListener(this);
+        findViewById(R.id.btn_4).setOnClickListener(this);
+        findViewById(R.id.btn_5).setOnClickListener(this);
+        findViewById(R.id.btn_6).setOnClickListener(this);
+        findViewById(R.id.btn_7).setOnClickListener(this);
+        findViewById(R.id.btn_8).setOnClickListener(this);
+        findViewById(R.id.btn_9).setOnClickListener(this);
+        findViewById(R.id.btn_c).setOnClickListener(this);
+        findViewById(R.id.btn_plus).setOnClickListener(this);
+        findViewById(R.id.btn_minus).setOnClickListener(this);
+        findViewById(R.id.btn_multiplier).setOnClickListener(this);
+        findViewById(R.id.btn_equals).setOnClickListener(this);
     }
 
     @Override
@@ -42,9 +67,88 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        // calculated result is displayed in this textView
+        result = (TextView)findViewById(R.id.result);
+
+        // switch according to button id
         switch(v.getId()){
-            case R.id.btn_0:
-                
+            // when c button is pushed
+            case R.id.btn_c:
+                // reset first, second and operator values
+                first = 0;
+                second = 0;
+                operator = -1;
+
+                // 0 is displayed as result
+                result.setText(Integer.toString(first));
+                // new input will be accepted
+                isNewInput = true;
+                break;
+            // when operators are pushed
+            case R.id.btn_multiplier:
+                // operator will be 2 eventually
+                operator++;
+            case R.id.btn_minus:
+                // operator will be 1
+                operator++;
+            case R.id.btn_plus:
+                // operator will be 0
+                operator++;
+                // store input as first value
+                first = Integer.parseInt(result.getText().toString());
+                // new input will be accepted
+                isNewInput = true;
+                break;
+            // when = is pushed
+            case R.id.btn_equals:
+                // store input as second value
+                second = Integer.parseInt(result.getText().toString());
+                // num will be displayed as result
+                int num = 0;
+                switch (operator){
+                    // plus
+                    case 0:
+                        num = first+second;
+                        break;
+                    // minus
+                    case 1:
+                        num = first-second;
+                        break;
+                    // multiplier
+                    case 2:
+                        num = first*second;
+                        break;
+                }
+                // check if operator was clicked
+                if(operator != -1) {
+                    // result is displayed
+                    String s = Integer.toString(num);
+                    result.setText(s);
+                    // reset operator
+                    operator = -1;
+                }
+                break;
+            // when a number button is pushed
+            default:
+                // get the number input as string
+                Button btn_num = (Button)v;
+                String number = btn_num.getText().toString();
+
+                if(isNewInput){
+                    // reset the result display
+                    result.setText(number);
+                    isNewInput = false;
+                }else{
+                    // concat past digit(s) with new input
+                    String past = result.getText().toString();
+                    // if first digit entered is 0, get rid of it
+                    if(past.equals("0")){
+                        result.setText(number);
+                    }else{
+                        result.setText(past+number);
+                    }
+                }
+                break;
         }
     }
 }
